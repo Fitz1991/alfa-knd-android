feb8d125452479d1c8779455544ea7dda4a19a48
petkun <petkun@keysystems.ru>
2019-06-04 08:59:01 +0300

    merge from master

--- app/src/main/java/ru/npc_ksb/alfaknd/MainActivity.kt
+++ app/src/main/java/ru/npc_ksb/alfaknd/MainActivity.kt
@@ -2,8 +2,6 @@ package ru.npc_ksb.alfaknd
 
 import android.content.Intent
 import android.os.Bundle
-import android.support.design.widget.NavigationView
-import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
@@ -11,8 +9,6 @@ import android.view.Menu
 import android.view.MenuItem
 import android.support.v4.widget.DrawerLayout
 import android.view.View
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.app_bar_main.*
 
 
 class MainActivity : AppCompatActivity() {
