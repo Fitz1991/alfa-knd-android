bd3f68ec222b2b131837c3fc28afa533595fbf90
Inessa <inessa397@mail.ru>
2019-06-07 10:50:39 +0300

    кнопка открытия меню

--- app/src/main/java/ru/npc_ksb/alfaknd/MainActivity.kt
+++ app/src/main/java/ru/npc_ksb/alfaknd/MainActivity.kt
@@ -2,13 +2,22 @@ package ru.npc_ksb.alfaknd
 
 import android.content.Intent
 import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentActivity
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
 import android.view.Menu
 import android.view.MenuItem
 import android.support.v4.widget.DrawerLayout
+import android.support.v4.widget.SlidingPaneLayout
+import android.util.Log
 import android.view.View
+import kotlinx.android.synthetic.main.content_main_margin_left.*
+import ru.npc_ksb.alfaknd.fragments.MasterFragment
+import android.support.v4.widget.SlidingPaneLayout.PanelSlideListener
+
+
 
 
 class MainActivity : AppCompatActivity() {
@@ -16,18 +25,28 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val mSlidingLayout: SlidingPaneLayout = findViewById(R.id.sliding_pane_layout)
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
-
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
-       // val navView: NavigationView = findViewById(R.id.fragment_master)
-        val toggle = ActionBarDrawerToggle(
-            this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close
-        )
-       // drawerLayout.addDrawerListener(toggle)
-            //toggle.syncState()
+        val toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        //val navView: FragmentActivity = findViewById(R.id.fragment_master)
 
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        toolbar.setNavigationOnClickListener {
+            if (mSlidingLayout.isOpen) {
+                mSlidingLayout.closePane()
+            } else {
+                mSlidingLayout.openPane()
+            }
+        }
     }
+
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -38,14 +57,10 @@ class MainActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
 
         val id = item.itemId
-
-        //noinspection SimplifiableIfStatement
         return if (id == R.id.action_settings) {
             true
         } else super.onOptionsItemSelected(item)
-
     }
-
 
     fun onQRCodeClick(v: View) {
         val intent = Intent(this, QRCodeActivity::class.java)
