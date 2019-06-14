b2f7919104e92d32104ed744ad1514f801501dd6
Inessa <inessa397@mail.ru>
2019-06-14 15:25:17 +0300

    создание таблицы

--- app/src/main/java/ru/npc_ksb/alfaknd/fragments/BlankFragment0.kt
+++ app/src/main/java/ru/npc_ksb/alfaknd/fragments/BlankFragment0.kt
@@ -16,23 +16,14 @@ import ru.npc_ksb.alfaknd.R
 class BlankFragment0 : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
         // Get the custom view for this fragment layout
         val view = inflater.inflate(R.layout.fragment_blank0,container,false)
 
-        // Get the text view widget reference from custom layout
-        val tv = view.findViewById<TextView>(R.id.text_view)
-
-        // Set a click listener for text view object
-        tv.setOnClickListener{
-            // Change the text color
-            tv.setTextColor(Color.RED)
-
-            // Show click confirmation
-            Toast.makeText(view.context,"TextView clicked.",Toast.LENGTH_SHORT).show()
-        }
 
         // Return the fragment view/layout
         return view
